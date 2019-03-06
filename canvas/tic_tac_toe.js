8c422bf839b48d2f80a57ca27f5e838148d34f52
Meynet Léo <44263847+Neexos@users.noreply.github.com>
2019-03-06 17:28:59 +0100

    Add files via upload

--- canvas/tic_tac_toe.js
+++ canvas/tic_tac_toe.js
@@ -7,10 +7,26 @@ for (i = 0; i< 10; i++) {
   }
 }
 
+var bomb = new Array(10);
+for(i=0; i<10; i++){
+  bomb[i] = new Array(10);
+  for(j=0; j<10; j++){
+    if(getRandomInt(2) == 1){
+    bomb[i][j] = "b";
+    }
+    else{
+      bomb[i][j] = "a";
+    } //affecte a ou b à une case. b = bomb | a = Pas de bomb
+  }
+}
+
 var id = document.getElementById("tic_tac_toe");
 var context = id.getContext("2d");
 tic_tac_toe();
 
+
+
+/---------------Fonction Creation--------------------/
 function tic_tac_toe() {
   context.fillStyle = "#bbbbbb" //couleur fond
   context.fillRect(0,0,1000,1000);
@@ -31,32 +47,41 @@ function tic_tac_toe() {
   }  
   context.stroke(); 
 
-  var bomb = new Array(10);
-    for(i=0; i<10; i++){
-    bomb[i] = new Array(2);
-      for(j=0; j<10; j++){
-        if(getRandomInt(2) == 1){
-          bomb[i][j] = "b";
-        }
-        else{
-          bomb[i][j] = "a";
-        } //affecte a ou b à une case. b = bomb | a = Pas de bomb
+  
+  for(i=0; i<10; i++){				//Dessin des bombes
+    for(j=0; j<10; j++){
+      if(bomb[i][j] == "b"){
+      context.strokeStyle =  "#ffffff";
+      context.fillStyle = "#ffffff"
+      context.lineWidth = 5;
+      context.beginPath();
+      context.arc((i*2+1)*50, (j*2+1)*50, 10, 0, 2*Math.PI);
+      context.fill();
+      context.stroke();
       }
     }
-
-    for(i=0; i<10; i++){
-      for(j=0; j<10; j++){
-          if(getRandomInt(2) == 1){
-            bomb[i][j] = "b";
-          }
-          else{
-            bomb[i][j] = "a";
-          } //affecte a ou b à une case. b = bomb | a = Pas de bomb
-        }
+  }
+  
+  for(i=0; i<10; i++){				//Boucles affichage chiffres
+    for(j=0; j<10; j++){
+      if(i==0){
+	
       }
-  
-  
+      else if(j==0){
+	
+      }
+      else if(i==9){
+	
+      }
+      else if(){
+	
+      }
+    }
+  }
 }
+
+
+
 
 function getRandomInt(max){
   return Math.floor(Math.random()*Math.floor(max));
@@ -75,7 +100,17 @@ function deux(x,y){
   context.fillStyle="#000000"
   context.font ="40px Arial";
   context.fillText("2",(x*2+1)*50,(y*2+1)*50);
-  context.stroke();
+  context.stroke();for(i=0; i<10; i++){
+    bomb[i] = new Array(2);
+      for(j=0; j<10; j++){
+        if(getRandomInt(2) == 1){
+          bomb[i][j] = "b";
+        }
+        else{
+          bomb[i][j] = "a";
+        } //affecte a ou b à une case. b = bomb | a = Pas de bomb
+      }
+    }
 }
 
 function trois(x,y){
